504bdc9eb81c2e407940409a364334fbcb73c87d
foiweuadf <foiweuadf@outlook.com>
2025-07-03 00:12:56 +0800

    Update worker.mjs

--- src/worker.mjs
+++ src/worker.mjs
@@ -84,12 +84,14 @@ async function doproxy(req) {
     console.log(response)
     let text = await response.text();
 
-    text = text.replace("var s = o.length;", "var s = o.length;let i=0;")
+    text = text.replace("var s=o.length;", "var s=o.length;let i=0;")
 
     text = text + 'function toNumbers(d){var e=[];d.replace(/(..)/g,function(d){e.push(parseInt(d,16))});return e}function toHex(){for(var d=[],d=1==arguments.length&&arguments[0].constructor==Array?arguments[0]:arguments,e="",f=0;f<d.length;f++)e+=(16>d[f]?"0":"")+d[f].toString(16);return e.toLowerCase()} var a=toNumbers("f655ba9d09a112d4968c63579db590b4"),b=toNumbers("98344c2eee86c3994890592585b49f80"),c=toNumbers("4f4c2cbaf6264e09f91c245ac70536db");const result = toHex(slowAES.decrypt(c,2,a,b));'
+    console.log(text)
+    
     let result = eval(text)
 
-    console.log(text)
+    
 // var slowAES = {
 // 	aes: {
 // 		keySize: {
